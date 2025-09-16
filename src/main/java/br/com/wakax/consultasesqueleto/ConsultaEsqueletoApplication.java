6b886932ae66485e6c936e5d196ddd97d64a6ba9
igoralves367 <112591719+igoralves367@users.noreply.github.com>
2025-09-16 19:42:52 -0300

    Adicionado configurações de senha e autenticação

--- src/main/java/br/com/wakax/consultasesqueleto/ConsultaEsqueletoApplication.java
+++ src/main/java/br/com/wakax/consultasesqueleto/ConsultaEsqueletoApplication.java
@@ -2,11 +2,18 @@ package br.com.wakax.consultasesqueleto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 @EnableFeignClients
 @RestController
 @RequestMapping("/")
