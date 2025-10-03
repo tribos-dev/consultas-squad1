aa59f330b567b6ba79f2b4926aaa4ae8417488df
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2025-10-03 17:15:17 -0300

    feat: implementa camada repository

--- src/main/java/br/com/wakax/consultasesqueleto/fipe/application/repository/FipeRepository.java
+++ src/main/java/br/com/wakax/consultasesqueleto/fipe/application/repository/FipeRepository.java
@@ -1,6 +1,11 @@
 package br.com.wakax.consultasesqueleto.fipe.application.repository;
 
-public interface FipeRepository {
+import br.com.wakax.consultasesqueleto.fipe.domain.Marca;
+import br.com.wakax.consultasesqueleto.fipe.domain.TipoVeiculo;
 
+import java.util.List;
+
+public interface FipeRepository {
+    List<Marca> listarMarcas(TipoVeiculo tipoVeiculo);
 }
 
--- src/main/java/br/com/wakax/consultasesqueleto/fipe/infra/FipeRepositoryFeign.java
+++ src/main/java/br/com/wakax/consultasesqueleto/fipe/infra/FipeRepositoryFeign.java
@@ -3,8 +3,13 @@ package br.com.wakax.consultasesqueleto.fipe.infra;
 import org.springframework.stereotype.Repository;
 
 import br.com.wakax.consultasesqueleto.fipe.application.repository.FipeRepository;
+import br.com.wakax.consultasesqueleto.fipe.domain.Marca;
+import br.com.wakax.consultasesqueleto.fipe.domain.TipoVeiculo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -13,5 +18,12 @@ public class FipeRepositoryFeign implements FipeRepository {
 
   private final FipeFeignClient fipeFeignClient;
 
+  @Override
+  public List<Marca> listarMarcas(TipoVeiculo tipoVeiculo) {
+    return fipeFeignClient.listarMarcas(tipoVeiculo.getValor())
+        .stream()
+        .sorted(Comparator.comparing(Marca::nome))
+        .toList();
+  }
 }
 
