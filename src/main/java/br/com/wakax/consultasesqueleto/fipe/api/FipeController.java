9aebbbf1c8ffca91f521e942c159066439671807
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2025-10-03 17:13:56 -0300

    feat: implementa camada controller

--- src/main/java/br/com/wakax/consultasesqueleto/fipe/api/FipeController.java
+++ src/main/java/br/com/wakax/consultasesqueleto/fipe/api/FipeController.java
@@ -3,8 +3,12 @@ package br.com.wakax.consultasesqueleto.fipe.api;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.wakax.consultasesqueleto.fipe.application.service.FipeService;
+import br.com.wakax.consultasesqueleto.fipe.domain.Marca;
+import br.com.wakax.consultasesqueleto.fipe.domain.TipoVeiculo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,5 +17,10 @@ public class FipeController implements FipeAPI {
 
   private final FipeService fipeService;
 
+  @Override
+  public List<Marca> listarMarcas(TipoVeiculo tipoVeiculo) {
+    return fipeService.listarMarcas(tipoVeiculo);
+  }
 }
+
 
