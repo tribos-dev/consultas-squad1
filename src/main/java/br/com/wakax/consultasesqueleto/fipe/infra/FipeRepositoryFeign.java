2705d2517c340380f885b5082e775e0e00997c34
welingtonalcantara <welingtonalcantara@gmail.com>
2025-10-13 11:21:39 -0300

    feat: implementa Repository Feign

--- src/main/java/br/com/wakax/consultasesqueleto/fipe/infra/FipeRepositoryFeign.java
+++ src/main/java/br/com/wakax/consultasesqueleto/fipe/infra/FipeRepositoryFeign.java
@@ -1,5 +1,6 @@
 package br.com.wakax.consultasesqueleto.fipe.infra;
 
+import br.com.wakax.consultasesqueleto.fipe.api.FipeApiResponseDTO;
 import br.com.wakax.consultasesqueleto.fipe.domain.TipoVeiculo;
 import br.com.wakax.consultasesqueleto.fipe.domain.Veiculo;
 import org.springframework.stereotype.Repository;
@@ -16,7 +17,26 @@ public class FipeRepositoryFeign implements FipeRepository {
   private final FipeFeignClient fipeFeignClient;
   @Override
   public Veiculo buscarValorVeiculo(TipoVeiculo tipoVeiculo, String idMarca, String idModelo, String idAno) {
-    return null;
+    log.info("Buscando valor do veículo na Fipe para Tipo: {}, Marca: {}, Modelo: {}, Ano: {}",
+            tipoVeiculo.getValor(), idMarca, idModelo, idAno);
+    FipeApiResponseDTO apiResponse = fipeFeignClient.buscarValorVeiculo(
+            idMarca,
+            idModelo,
+            idAno,
+            tipoVeiculo.getValor()
+    );
+    Veiculo veiculoDominio = new Veiculo(
+            apiResponse.tipoVeiculo(),
+            apiResponse.valor(),
+            apiResponse.marca(),
+            apiResponse.modelo(),
+            apiResponse.anoModelo(),
+            apiResponse.combustivel(),
+            apiResponse.codigoFipe(),
+            apiResponse.mesReferencia(),
+            apiResponse.siglaCombustivel()
+    );
+    return veiculoDominio;
   }
 }
 
