1f0b2bc7aea02cf5bc074229ae9fbd69930cf5b8
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2025-10-14 22:28:56 -0300

    implementa camada controller

--- src/main/java/br/com/wakax/consultasesqueleto/fipe/api/FipeAPI.java
+++ src/main/java/br/com/wakax/consultasesqueleto/fipe/api/FipeAPI.java
@@ -1,11 +1,20 @@
 package br.com.wakax.consultasesqueleto.fipe.api;
 
+import br.com.wakax.consultasesqueleto.fipe.domain.Marca;
+import br.com.wakax.consultasesqueleto.fipe.domain.TipoVeiculo;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/fipe")
 public interface FipeAPI {
+
+    @GetMapping("/marcas")
+    List<Marca>listarMarcas(@RequestParam TipoVeiculo tipoVeiculo);
 
 }
 
--- src/main/java/br/com/wakax/consultasesqueleto/fipe/api/FipeController.java
+++ src/main/java/br/com/wakax/consultasesqueleto/fipe/api/FipeController.java
@@ -1,10 +1,14 @@
 package br.com.wakax.consultasesqueleto.fipe.api;
 
+import br.com.wakax.consultasesqueleto.fipe.domain.Marca;
+import br.com.wakax.consultasesqueleto.fipe.domain.TipoVeiculo;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.wakax.consultasesqueleto.fipe.application.service.FipeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,5 +17,12 @@ public class FipeController implements FipeAPI {
 
   private final FipeService fipeService;
 
+  @Override
+  public List<Marca> listarMarcas(TipoVeiculo tipoVeiculo) {
+    log.info("[start] FipeController - listarMarcas");
+    List<Marca> marcas = fipeService.listarMarcas(tipoVeiculo);
+    log.info("[finish] FipeController - listarMarcas");
+    return marcas;
+  }
 }
 
