58f7fa61be436e2c87262625ff7e0f7f89254af3
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2025-10-14 22:29:52 -0300

    feat: implementa camada repository

--- src/main/java/br/com/wakax/consultasesqueleto/fipe/application/repository/FipeRepository.java
+++ src/main/java/br/com/wakax/consultasesqueleto/fipe/application/repository/FipeRepository.java
@@ -1,6 +1,12 @@
 package br.com.wakax.consultasesqueleto.fipe.application.repository;
 
+import br.com.wakax.consultasesqueleto.fipe.domain.Marca;
+import br.com.wakax.consultasesqueleto.fipe.domain.TipoVeiculo;
+
+import java.util.List;
+
 public interface FipeRepository {
 
+    List<Marca> listarMarcas(TipoVeiculo tipoVeiculo);
 }
 
--- src/main/java/br/com/wakax/consultasesqueleto/fipe/infra/FipeRepositoryFeign.java
+++ src/main/java/br/com/wakax/consultasesqueleto/fipe/infra/FipeRepositoryFeign.java
@@ -1,10 +1,18 @@
 package br.com.wakax.consultasesqueleto.fipe.infra;
 
+import br.com.wakax.consultasesqueleto.fipe.domain.Marca;
+import br.com.wakax.consultasesqueleto.fipe.domain.TipoVeiculo;
+import br.com.wakax.consultasesqueleto.handler.APIException;
+import br.com.wakax.consultasesqueleto.handler.ErrorCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import br.com.wakax.consultasesqueleto.fipe.application.repository.FipeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -13,5 +21,18 @@ public class FipeRepositoryFeign implements FipeRepository {
 
   private final FipeFeignClient fipeFeignClient;
 
+  @Override
+  public List<Marca> listarMarcas(TipoVeiculo tipoVeiculo) {
+    log.info("[start] FipeRepositoryFeign - listarMarcas");
+    List<Marca> marcas = fipeFeignClient.listarMarcas(tipoVeiculo.getValor())
+        .stream()
+        .toList();
+    if (marcas == null || marcas.isEmpty()) {
+      throw new APIException(HttpStatus.NOT_FOUND, ErrorCode.FIPE_DADOS_NAO_ENCONTRADOS);
+    }
+    log.info("[finish] FipeRepositoryFeign - listarMarcas");
+    return marcas;
+
+  }
 }
 
