0c3d072782c31ca4db6243bb29ab957d432cd0a8
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2025-10-14 22:29:16 -0300

    implementa camada service

--- src/main/java/br/com/wakax/consultasesqueleto/fipe/application/service/FipeApplicationService.java
+++ src/main/java/br/com/wakax/consultasesqueleto/fipe/application/service/FipeApplicationService.java
@@ -1,10 +1,14 @@
 package br.com.wakax.consultasesqueleto.fipe.application.service;
 
+import br.com.wakax.consultasesqueleto.fipe.domain.Marca;
+import br.com.wakax.consultasesqueleto.fipe.domain.TipoVeiculo;
 import org.springframework.stereotype.Service;
 
 import br.com.wakax.consultasesqueleto.fipe.application.repository.FipeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +17,12 @@ public class FipeApplicationService implements FipeService {
 
   private final FipeRepository fipeRepository;
 
+  @Override
+  public List<Marca> listarMarcas(TipoVeiculo tipoVeiculo) {
+    log.info("[start] FipeApplicationService - listarMarcas");
+    List<Marca> marcas = fipeRepository.listarMarcas(tipoVeiculo);
+    log.info("[finish] FipeApplicationService - listarMarcas");
+    return marcas;
+  }
 }
 
--- src/main/java/br/com/wakax/consultasesqueleto/fipe/application/service/FipeService.java
+++ src/main/java/br/com/wakax/consultasesqueleto/fipe/application/service/FipeService.java
@@ -1,6 +1,12 @@
 package br.com.wakax.consultasesqueleto.fipe.application.service;
 
+import br.com.wakax.consultasesqueleto.fipe.domain.Marca;
+import br.com.wakax.consultasesqueleto.fipe.domain.TipoVeiculo;
+
+import java.util.List;
+
 public interface FipeService {
 
+    List<Marca> listarMarcas(TipoVeiculo tipoVeiculo);
 }
 
