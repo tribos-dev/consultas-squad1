e4db638095547f9fcedcb28cedb41d3f604c6b9d
Ronan Lima <ronanlimasilva@hotmail.com>
2025-11-05 21:09:28 -0300

    implementacao no fluxo

---
+++ src/main/java/br/com/wakax/consultasesqueleto/fipe/application/FipeApplication.java
@@ -0,0 +1,12 @@
+package br.com.wakax.consultasesqueleto.fipe.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FipeApplication.class, args);
+    }
+}
+
